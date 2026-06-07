baf9d03c1762961a7ef47ab7e4e25a36b4ed7574
Karlo Prukner <karloprukner05@gmail.com>
2026-06-07 18:12:22 +0100

    Added initial user class

---
+++ flaskr/db/user.py
@@ -0,0 +1,27 @@
+class User:
+    
+    def __init__(self, id, email, password, f_name, l_name, address):
+        self._id        = id
+        self._email     = email
+        self._password  = password
+        self._f_name    = f_name
+        self._l_name    = l_name
+        self._address   = address
+
+    def get_id(self):
+        return self._id
+
+    def get_email(self):
+        return self._email
+    
+    def get_password(self):
+        return self._password
+
+    def get_f_name(self):
+        return self._f_name
+
+    def get_l_name(self):
+        return self._l_name
+
+    def get_address(self):
+        return self._address
