66fa7c887e385e4b02759afccf680c6e074a60c6
Karlo Prukner <karloprukner05@gmail.com>
2026-06-07 21:09:10 +0100

    Added a return to the load_file function

--- flaskr/db/db.py
+++ flaskr/db/db.py
@@ -5,12 +5,10 @@ def is_file_empty(file_path):
     return os.path.getsize(file_path) == 0
 
 def load_file(json_file):
-    
-    print("I am ran")
+
     if not is_file_empty(json_file):
         with open(json_file, "r") as file:
-            print("I am used")
-            print(json.load(file))
+            return json.load(file)
 
 def save_to_file(file, json_data):
     with open(file, "a") as json_file:
