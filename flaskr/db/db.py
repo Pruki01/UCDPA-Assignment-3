f934f685e60c66a22236a30309f5e1bc4aa48833
Karlo Prukner <karloprukner05@gmail.com>
2026-06-07 21:02:26 +0100

    Fixed typo, now is_file_empty returns a boolean

--- flaskr/db/db.py
+++ flaskr/db/db.py
@@ -2,12 +2,14 @@ import json
 import os
 
 def is_file_empty(file_path):
-    return os.path.getsize(file_path)
+    return os.path.getsize(file_path) == 0
 
 def load_file(json_file):
     
+    print("I am ran")
     if not is_file_empty(json_file):
         with open(json_file, "r") as file:
+            print("I am used")
             print(json.load(file))
 
 def save_to_file(file, json_data):
