068e67cea2488cad570486f83e5d66035fc5f63e
Karlo Prukner <karloprukner05@gmail.com>
2026-06-08 19:18:48 +0100

    Modified some data to test

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,4 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from flaskr import routes
+from . import routes
+from .db import book, db, user
+
+
+book1 = book.Book('Book1', 'Author1', 'Genre1', 'Qty1')
+book2 = book.Book('Book2', 'Author2', 'Genre2', 'Qty2')
+
+print(db.load_users())
+print(db.load_books())
+books = db.load_file('flaskr/db/books.json')['books']
+print(books)
