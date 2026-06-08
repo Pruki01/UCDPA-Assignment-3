af058eeb114f9c43b0cb914c946dc8d920513d8f
Karlo Prukner <karloprukner05@gmail.com>
2026-06-08 08:16:45 +0100

    Added save_books function

--- flaskr/db/db.py
+++ flaskr/db/db.py
@@ -9,7 +9,15 @@ def load_file(json_file):
     if not is_file_empty(json_file):
         with open(json_file, "r") as file:
             return json.load(file)
-
 def save_to_file(file, json_data):
     with open(file, "a") as json_file:
         json.dump(json_data, json_file)
+    
+def save_books(json_file):
+    return {
+        "users": [json_file]
+    }
+
+
+
+    
