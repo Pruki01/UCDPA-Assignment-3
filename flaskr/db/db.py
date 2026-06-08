5442d02d246f58fad678235cbf44b3be13f47346
Karlo Prukner <karloprukner05@gmail.com>
2026-06-08 08:21:18 +0100

    Pickle module comment added

--- flaskr/db/db.py
+++ flaskr/db/db.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+#Maybe use pickle module?
+
 def is_file_empty(file_path):
     return os.path.getsize(file_path) == 0
 
@@ -10,7 +12,7 @@ def load_file(json_file):
         with open(json_file, "r") as file:
             return json.load(file)
 def save_to_file(file, json_data):
-    with open(file, "a") as json_file:
+    with open(file, "w") as json_file:
         json.dump(json_data, json_file)
     
 def save_books(json_file):
