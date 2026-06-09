7960ba93ffbdb6f5028192ac3d2fee09b9788a7c
Karlo Prukner <karloprukner05@gmail.com>
2026-06-09 09:08:03 +0100

    Modified routes to render index template

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,5 +1,14 @@
+from flask import render_template
 from flaskr import app
+from flaskr.db import db, book
+
+book1 = book.Book('Title', 'Author', 'Genre', 'QTY').to_json()
+book2 = book.Book('Title1', 'Author1', 'Genre1', 'QTY1').to_json()
+book3 = book.Book('Title2', 'Author2', 'Genre2', 'QTY2').to_json()
+
+books_json = db.load_books()
+books = books_json['books']
 
 @app.route("/")
 def hello_wolrd():
-    return "Hello world!"
+    return render_template('index.html', book_list=books)
