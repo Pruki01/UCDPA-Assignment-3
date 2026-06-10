f8b5de8126cb298794dc1fbb122bfec1f2d8c471
Karlo Prukner <karloprukner05@gmail.com>
2026-06-10 15:38:12 +0100

    Added an image property to the book class

--- flaskr/db/book.py
+++ flaskr/db/book.py
@@ -1,10 +1,11 @@
 from uuid import uuid4
 class Book:
 
-    def __init__(self, title, author, genre, qty):
+    def __init__(self, title, author, img, genre, qty):
         self._isbn      = uuid4()
         self._title     = title 
         self._author    = author
+        self._img       = img
         self._genre     = genre
         self._qty       = qty
 
@@ -17,6 +18,9 @@ class Book:
     def get_author(self):
         return self._author
 
+    def get_img(self):
+        return self._img
+
     def get_genre(self):
         return self._genre
 
@@ -27,6 +31,7 @@ class Book:
         print(f"ISBN: {self._isbn}")
         print(f"Title: {self._title}")
         print(f"Author: {self._author}")
+        print(f"Image: {self._img}")
         print(f"Genre: {self._genre}")
         print(f"Quantity: {self._qty}")
 
@@ -35,6 +40,7 @@ class Book:
             "ISBN": str(self._isbn),
             "Title": self._title,
             "Author": self._author,
+            "Image": self._img,
             "Genre": self._genre,
             "Qty": self._qty
             }
