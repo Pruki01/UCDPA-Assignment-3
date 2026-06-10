c4eb64ef561bbbf1d9c25c66238c3f94739253e0
Karlo Prukner <karloprukner05@gmail.com>
2026-06-10 15:37:42 +0100

    Removed unecessary code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,13 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 
 from . import routes
-from .db import book, db, user
 
-
-book1 = book.Book('Book1', 'Author1', 'Genre1', 'Qty1')
-book2 = book.Book('Book2', 'Author2', 'Genre2', 'Qty2')
-
-print(db.load_users())
-print(db.load_books())
-books = db.load_file('flaskr/db/books.json')['books']
-print(books)
