69693943a545f629a91b8075d5010e86a0f5129b
Karlo Prukner <karloprukner05@gmail.com>
2026-06-12 21:49:01 +0100

    Moved all routes to app.py

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 from flaskr import app
+from flaskr.db import db, user, book
+from flask import(
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+@app.route('/')
+def index():
+    print(db.load_books())
+    return render_template('index.html', book_list=db.load_books())
+
+@app.route('/<uuid:ISBN>')
+def book_page(ISBN):
+    books = db.load_books()
+    book = db.load_book(ISBN, books)
+
+    if book is None:
+        flash('The book you are looking for doesn\'t exist!')
+    else:
+        return render_template('book.html', book=book)
+
+@app.route('/register')
+def register():
+    return render_template('auth/register.html')
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
