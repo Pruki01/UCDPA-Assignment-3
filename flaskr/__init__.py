3649fa2635665be26de5617f82987d9f716410dd
Karlo Prukner <karloprukner05@gmail.com>
2026-06-13 11:05:21 +0100

    Added secret key

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,9 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+print(app.config['SECRET_KEY'])
