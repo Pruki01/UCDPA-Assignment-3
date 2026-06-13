9399590e24e5b4f6484606793ffd6fee6eafe2b1
Karlo Prukner <karloprukner05@gmail.com>
2026-06-13 20:47:13 +0100

    Removed print statements

--- app.py
+++ app.py
@@ -91,39 +91,48 @@ def logout():
 @app.route('/register_staff', methods=['GET','POST'])
 def register_staff():
 
-    if request.method == 'POST':
-        email       = request.form['email']
-        password    = generate_password_hash(request.form['password'])
-        f_name      = request.form['first_name']
-        l_name      = request.form['last_name']
-        address     = request.form['address']
+    if session['user_type'] == 'Staff':
+        if request.method == 'POST':
+            email       = request.form['email']
+            password    = generate_password_hash(request.form['password'])
+            f_name      = request.form['first_name']
+            l_name      = request.form['last_name']
+            address     = request.form['address']
 
-        db.add_user(user.User(
-            email,
-            password,
-            f_name,
-            l_name,
-            address,
-            user.UserType.Staff
-        ))
+            db.add_user(user.User(
+                email,
+                password,
+                f_name,
+                l_name,
+                address,
+                user.UserType.Staff
+            ))
 
-        return redirect(url_for('login'))
+            return redirect(url_for('login'))
+    else:
+        return redirect(url_for('index'))
+
     return render_template('auth/staff_register.html')
 
 @app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
-    if request.method == 'POST':
-        title   = request.form['title']
-        author  = request.form['author']
-        image   = request.form['image']
-        genre   = request.form['genre']
-        desc    = request.form['description']
 
-        new_book = book.Book(title, author, image, genre, desc, 0)
+    if session['user_type'] == 'Staff':
+        if request.method == 'POST':
+            title   = request.form['title']
+            author  = request.form['author']
+            image   = request.form['image']
+            genre   = request.form['genre']
+            desc    = request.form['description']
 
-        if new_book:
-            db.add_book(new_book)
-            return redirect(url_for('index'))
+            new_book = book.Book(title, author, image, genre, desc, 0)
+
+            if new_book:
+                db.add_book(new_book)
+                return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
     return render_template('add_book.html')
 
 if __name__ == "__main__":
